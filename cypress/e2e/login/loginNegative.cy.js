0901a142718e3e0627cf294c86bae86bd45c57f7
kshitizrajan7 <kshitizrajandev@gmail.com>
2025-11-13 18:05:46 +0545

    add negative login tests and remove demo JS files

--- cypress/e2e/login/loginNegative.cy.js
+++ cypress/e2e/login/loginNegative.cy.js
@@ -16,9 +16,35 @@ describe('Sauce demo login test', () => {
     cy.get('#login-button').click() //click login
 
     //it will give the error message 
-    // catching error message and assert it is visible and correct.
+    // selecting error message and asserting it is visible and correct.
     cy.get('[data-test = "error"]').should('be.visible')
       .and('contain.text', 'Epic sadface: Sorry, this user has been locked out.')
   })
 
+  //invalid username with valid password
+  it("Invalid username should show error message", () => {
+    cy.get('#user-name').type('invalid_user') // invalid username
+    cy.get('#password').type('secret_sauce') // valid password
+    cy.get('#login-button').click();
+    cy.get('[data-test="error"]').should('be.visible')
+      .and('contain.text', 'Epic sadface: Username and password do not match any user in this service')
+  })
+
+  //invalid password with valid username
+  it('Invalid password should show error message', () => {
+    cy.get('#user-name').type('standard_user'); // valid username
+    cy.get('#password').type('invalidPassword'); // invalid password
+    cy.get('#login-button').click();
+
+    cy.get('[data-test="error"]').should('be.visible')
+      .and('contain.text', 'Epic sadface: Username and password do not match any user in this service')
+  })
+
+  //Empty Username and password field
+  it('Empty username and password should show error', () => {
+    cy.get('#login-button').click();
+
+    cy.get('[data-test="error"]').should('be.visible')
+      .and('contain.text', 'Epic sadface: Username is required')
+  })
 })
