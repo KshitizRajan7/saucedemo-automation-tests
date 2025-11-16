ab7cf1c10d8fca532ce27390fe2d30f621208e24
kshitizrajan7 <kshitizrajandev@gmail.com>
2025-11-16 19:16:03 +0545

    Implement complete Practice Form test with Page Object, fixtures, and modal assertions

---
+++ cypress/pages/Practice.FormPage.js
@@ -0,0 +1,19 @@
+import 'cypress-file-upload';
+const page = {
+  firstName: '#firstName',
+  lastName: '#lastName',
+  email: '#userEmail',
+  genderMale: 'label[for="gender-radio-1"]',
+  phone: '#userNumber',
+  dob: '#dateOfBirthInput',
+  subjects: '#subjectsInput',
+  hobbiesSports: 'label[for="hobbies-checkbox-1"]',
+  picture: '#uploadPicture',
+  address: '#currentAddress',
+  state: '#state',
+  city: '#city',
+  dropdownOption: 'div[id^="react-select"][id*="-option-"]',
+  submit: '#submit'
+};
+
+export default page;
