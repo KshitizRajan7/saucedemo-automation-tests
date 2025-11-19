3b304e4d96d853e7d55e1b51043725ad194eb65b
kshitizrajan7 <kshitizrajandev@gmail.com>
2025-11-19 14:25:00 +0545

    create standalone SauceDemo Cypress project

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // baseUrl: "https://demoqa.com", // default base URL
+    baseUrl: "https://saucedemo.com", // default base URL
     env: {
-      demoUrl: "https://demoqa.com",
-      sauceUrl: "https://www.saucedemo.com",
       saucePassword: "secret_sauce"
     },
     setupNodeEvents(on, config) {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,35 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import sauceDemoPage from "../pages/sauceDemoPage";
+import sauceDemoPage from "../pages/sauceDemoPage"; 
 import page from "../pages/sauceDemoPage";
-
-Cypress.Commands.add('assertModalField', (actualValue, expectedValue) => {
-  cy.get('td').contains(actualValue).siblings('td').should('have.text', expectedValue);
-});
 
 // Custom command for login for per user
 Cypress.Commands.add('login', (username, password) => {
